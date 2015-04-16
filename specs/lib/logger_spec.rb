0b3b48d1aed3e5cebcaa9f9d8921733366135b74
Julien Ammous <schmurfy@gmail.com>
2015-04-16 10:09:52 +0200

    hash syntax

--- specs/lib/logger_spec.rb
+++ specs/lib/logger_spec.rb
@@ -8,11 +8,11 @@ describe "Logger" do
   should "dispatch msg with its to registered outputs" do
     
     handler = mock('Handler')
-    handler.expects(:dispatch).with("the message", :id => 43, :user => 'bob')
+    handler.expects(:dispatch).with("the message", id: 43, user: 'bob')
     
     @logger.add_output(handler)
     
-    @logger.log("the message", :id => 43, :user => "bob")
+    @logger.log("the message", id: 43, user: "bob")
   end
   
   should 'mimic standard ruby logger interface' do
@@ -31,18 +31,18 @@ describe "Logger" do
   
   describe 'with hash context' do
     should 'dispatch msg and merged data' do
-      @logger.expects(:dispatch).with('msg', :id => 43, :user => 'bob')
+      @logger.expects(:dispatch).with('msg', id: 43, user: 'bob')
     
       @logger.with_context(:user => 'bob') do
-        @logger.log('msg', :id => 43)
+        @logger.log('msg', id: 43)
       end
     end
     
     should 'allow proc as value' do
-      @logger.expects(:dispatch).with('msg', :id => 43, :user => 'john')
+      @logger.expects(:dispatch).with('msg', id: 43, user: 'john')
       
-      @logger.with_context(:user => ->{ "john" } ) do
-        @logger.log('msg', :id => 43)
+      @logger.with_context(user: ->{ "john" } ) do
+        @logger.log('msg', id: 43)
       end
       
     end
@@ -72,10 +72,10 @@ describe "Logger" do
         end
       end
       
-      @logger.expects(:dispatch).with('msg', :id => 56, :user => 'alice')
+      @logger.expects(:dispatch).with('msg', id: 56, user: 'alice')
     
       @logger.with_context(ext) do
-        @logger.log('msg', :id => 56)
+        @logger.log('msg', id: 56)
       end
     end
     
@@ -83,11 +83,11 @@ describe "Logger" do
   
   describe 'with multiple contexts' do
     should 'merge the contexts in defined order (last defined has greater priority)' do
-      @logger.expects(:dispatch).with('msg', :operation => 'destroy', :user => 'billy')
+      @logger.expects(:dispatch).with('msg', operation: 'destroy', user: 'billy')
       
-      @logger.with_context(:user => 'bob') do
-        @logger.with_context(:user => 'billy') do
-          @logger.log('msg', :operation => "destroy")
+      @logger.with_context(user: 'bob') do
+        @logger.with_context(user: 'billy') do
+          @logger.log('msg', operation: "destroy")
         end
       end
       
